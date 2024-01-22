d6c7abadd1325916d1e521c71e9006fb64cc76a6
Shybert <25749775+shybert@users.noreply.github.com>
2024-01-22 13:32:03 +0100

    Add undo extension to Telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -323,6 +323,19 @@ local plugins = {
             require('telescope').load_extension('fzf')
         end
     },
+    {
+        "debugloop/telescope-undo.nvim",
+        keys = {
+            {
+                "<leader>u",
+                "<cmd>Telescope undo<cr>",
+                desc = "undo history",
+            },
+        },
+        config = function()
+            require("telescope").load_extension("undo")
+        end
+    },
 
     -- Diagnostics
     {
