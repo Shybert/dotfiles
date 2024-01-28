0156358b12054919a1dff653422e65cf54dc214e
Shybert <25749775+shybert@users.noreply.github.com>
2024-01-29 00:04:33 +0100

    Remove plugin for undo tree
    Didn't really use it...

--- nvim/init.lua
+++ nvim/init.lua
@@ -317,19 +317,6 @@ local plugins = {
             require('telescope').load_extension('fzf')
         end
     },
-    {
-        "debugloop/telescope-undo.nvim",
-        keys = {
-            {
-                "<leader>u",
-                "<cmd>Telescope undo<cr>",
-                desc = "undo history",
-            },
-        },
-        config = function()
-            require("telescope").load_extension("undo")
-        end
-    },
 
     -- Diagnostics
     {
