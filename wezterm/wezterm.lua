484d3b87e87eb0ce6db1f39406d44c7beb89fc26
Shybert <25749775+shybert@users.noreply.github.com>
2025-08-13 17:55:09 +0200

    Switch to Dracula theme for Wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Campbell (Gogh)'
+config.color_scheme = 'Dracula'
 config.default_prog = { "fish" }
 config.window_decorations = "TITLE | RESIZE"
 config.font_size = 13
