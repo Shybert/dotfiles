6246e033bbe8db78d5a2611bd8b6ffafc20ccf1c
Shybert <25749775+shybert@users.noreply.github.com>
2025-08-16 09:02:40 +0200

    Docker abbreviations for Fish

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,3 +1,7 @@
 abbr --add gs "git status"
 abbr --add gco "git checkout"
 abbr --add gl "git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+abbr --add dcu "docker compose up"
+abbr --add dcud "docker compose up -d"
+abbr --add dcd "docker compose down"
