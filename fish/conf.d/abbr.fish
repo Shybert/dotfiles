89a7f9254c3a9e6fd5de5f30e73036a90096084a
Shybert <25749775+shybert@users.noreply.github.com>
2025-08-16 09:01:54 +0200

    Fish abbreviations

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,3 @@
+abbr --add gs "git status"
+abbr --add gco "git checkout"
+abbr --add gl "git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
