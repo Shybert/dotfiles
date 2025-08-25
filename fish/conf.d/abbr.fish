137e8b0e678b3f931c6e15e7cd1314191c76d643
Sivert Olstad <sivert.olstad@nhn.no>
2025-08-25 07:57:44 +0200

    More Fish abbreviations

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,7 +1,11 @@
 abbr --add gs "git status"
 abbr --add gco "git checkout"
 abbr --add gl "git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+abbr --add gf "git fetch"
 
 abbr --add dcu "docker compose up"
 abbr --add dcud "docker compose up -d"
 abbr --add dcd "docker compose down"
+
+abbr --add lg "lazygit"
+abbr --add lzd "lazydocker"
