9edb8605e73c43272930230899e5d6fd4601bdf9
Sivert Olstad <sivert.olstad@nhn.no>
2025-09-02 08:50:21 +0200

    Another slight bufferline improvement

--- nvim/init.lua
+++ nvim/init.lua
@@ -402,9 +402,14 @@ local plugins = {
             options = {
                 diagnostics = "nvim_lsp",
                 separator_style = "slant",
-                diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                    local icon = level:match("error") and " " or " "
-                    return " " .. icon .. count
+                diagnostics_indicator = function(_, _, diagnostics_dict, _)
+                    local s = " "
+                    for e, n in pairs(diagnostics_dict) do
+                        local sym = e == "error" and " "
+                            or (e == "warning" and " " or " ")
+                        s = s .. n .. sym
+                    end
+                    return s
                 end
             },
         },
