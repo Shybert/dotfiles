5974ffb6c133e94ab564b090f04cef51882e0506
Sivert Olstad <sivert.olstad@nhn.no>
2025-09-05 13:12:51 +0200

    Git abbreviation, Fish

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,3 +1,4 @@
+abbr --add g "git"
 abbr --add gs "git status"
 abbr --add gco "git checkout"
 abbr --add gl "git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
