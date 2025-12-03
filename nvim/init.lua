f0c17f4a1716ea7be4ac0a4f55e9852b9c204545
Sivert Olstad <sivert.olstad@nhn.no>
2025-12-03 11:50:48 +0100

    Navigate between Git hunks

--- nvim/init.lua
+++ nvim/init.lua
@@ -509,7 +509,32 @@ local plugins = {
     -- Git integration
     {
         "lewis6991/gitsigns.nvim",
-        opts = {},
+        opts = {
+            on_attach = function(buffer)
+                local gitsigns = package.loaded.gitsigns
+
+                local function map(mode, l, r, desc)
+                    vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
+                end
+
+                map('n', ']c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ ']c', bang = true })
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end)
+
+                map('n', '[c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ '[c', bang = true })
+                    else
+                        gitsigns.nav_hunk('prev')
+                    end
+                end)
+            end,
+        }
+
     },
 
     -- Virtual environment
