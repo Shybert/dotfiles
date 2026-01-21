658c20f369237d93f2e6d0eb443a3085fb3b462c
Sivert Olstad <sivert.olstad@nhn.no>
2026-01-21 11:43:46 +0100

    Function for decoding JWT tokens

---
+++ fish/functions/jwt_decode.fish
@@ -0,0 +1,6 @@
+function jwt-decode
+    # Remove "Bearer " prefix if it exists (case-insensitive)
+    set -l token (string replace -ir '^Bearer\s+' '' -- $argv[1])
+
+    echo $token | jq -R 'split(".") | .[0:2] | map(gsub("-"; "+") | gsub("_"; "/") | gsub("%3D"; "=") | @base64d) | map(fromjson)'
+end
